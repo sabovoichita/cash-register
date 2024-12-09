8f609f335e911ed042374825e416c4c3bca86855
Voichita Maria <sabo_voichita@yahoo.com>
2024-12-09 14:54:59 +0100

    declared const cash, changeDue, purchaseBtn

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Welcome!");
+let price = 1.87;
+let cid = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100],
+];
+const cash = document.querySelector("#cash");
+const changeDue = document.querySelector("#change-due");
+const purchaseBtn = document.querySelector("#purchase-btn");
